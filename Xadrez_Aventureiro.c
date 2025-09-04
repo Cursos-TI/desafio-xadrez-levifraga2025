f16a3cfe944a4d3898a707c098a37bd11d66fb62
levifraga2025 <jrtagima@gmail.com>
2025-09-04 17:36:20 +0000

    iniciando nivel aventureiro

---
+++ Xadrez_Aventureiro.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    // ----------------------------
+    // Simulação de movimentos de xadrez
+    // ----------------------------
+
+    // Número de casas a mover
+    int casas_torre = 5;
+    int casas_bispo = 5;
+    int casas_rainha = 8;
+
+    // ----------------------------
+    // Movimento da Torre (for)
+    // Move-se 5 casas para a direita
+    // ----------------------------
